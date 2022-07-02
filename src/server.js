ad7addcec67cf16622061421f07eccc5467c9537
matheusjkl1 <matheus.unilog@gmail.com>
2022-07-02 16:02:39 -0300

    test ping

--- src/server.js
+++ src/server.js
@@ -5,6 +5,47 @@ import app from './app.js';
 const PORT = process.env.PORT || 3050;
 // Parse request body and verifies incoming requests using discord-interactions package
 
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken('token');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('token');
+
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
 
